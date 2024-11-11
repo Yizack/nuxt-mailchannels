dd0cc9a45a7d2f83abca76115ef47766c88b26b6
Yizack Rangel <yizackr@gmail.com>
2024-11-11 17:17:41 -0500

    docs(types): add comments

--- src/runtime/sdk/types/emails.ts
+++ src/runtime/sdk/types/emails.ts
@@ -1,5 +1,11 @@
 export interface MailChannelsEmailRecipient {
+  /**
+   * The email address of the recipient
+   */
   email: string
+  /**
+   * The name of the recipient
+   */
   name?: string
 }
 
@@ -10,8 +16,17 @@ export interface MailChannelsEmailContent {
 }
 
 export interface MailChannelsEmailAttachment {
+  /**
+   * The attachment data, encoded in base64
+   */
   content: string
+  /**
+   * The name of the attachment file
+   */
   filename: string
+  /**
+   * The MIME type of the attachment
+   */
   type: string
 }
 
@@ -47,13 +62,92 @@ export interface MailChannelsEmailSend {
 }
 
 export interface MailChannelsEmailOptions {
+  /**
+   * An array of attachments to be sent with the email
+   */
   attachments?: MailChannelsEmailAttachment[]
+  /**
+   * An array of `bcc` recipient objects, a single recipient object, or a single email address.
+   * @example
+   * bcc: [
+   *   { email: 'email1@example.com', name: 'Example1' },
+   *   { email: 'email2@example.com', name: 'Example2' },
+   * ]
+   * @example
+   * bcc: { email: 'email@example.com', name: 'Example' }
+   * @example
+   * bcc: 'email@example.com'
+   */
   bcc?: MailChannelsEmailRecipient[] | MailChannelsEmailRecipient | string
+  /**
+   * An array of `cc` recipient objects, a single recipient object, or a single email address.
+   * @example
+   * cc: [
+   *   { email: 'email1@example.com', name: 'Example1' },
+   *   { email: 'email2@example.com', name: 'Example2' },
+   * ]
+   * @example
+   * cc: { email: 'email@example.com', name: 'Example' }
+   * @example
+   * cc: 'email@example.com'
+   */
   cc?: MailChannelsEmailRecipient[] | MailChannelsEmailRecipient | string
+  /**
+   * An array of `from` recipient objects, a single recipient object, or a single email address.
+   * @example
+   * from: [
+   *   { email: 'email1@example.com', name: 'Example1' },
+   *   { email: 'email2@example.com', name: 'Example2' },
+   * ]
+   * @example
+   * from: { email: 'email@example.com', name: 'Example' }
+   * @example
+   * from: 'email@example.com'
+   */
   from: MailChannelsEmailRecipient | string
+  /**
+   * An array of `to` recipient objects, a single recipient object, or a single email address.
+   * @example
+   * to: [
+   *   { email: 'email1@example.com', name: 'Example1' },
+   *   { email: 'email2@example.com', name: 'Example2' },
+   * ]
+   * @example
+   * to: { email: 'email@example.com', name: 'Example' }
+   * @example
+   * to: 'email@example.com'
+   */
   to: MailChannelsEmailRecipient[] | MailChannelsEmailRecipient | string
+  /**
+   * An array of `reply_to` recipient objects, a single recipient object, or a single email address.
+   * @example
+   * reply_to: [
+   *   { email: 'email1@example.com', name: 'Example1' },
+   *   { email: 'email2@example.com', name: 'Example2' },
+   * ]
+   * @example
+   * reply_to: { email: 'email@example.com', name: 'Example' }
+   * @example
+   * reply_to: 'email@example.com'
+   */
   reply_to?: MailChannelsEmailRecipient[] | MailChannelsEmailRecipient | string
+  /**
+   * The subject of the email
+   */
   subject: string
+  /**
+   * The content of the email
+   */
   html: string
+  /**
+   * Data to be used if the email is a mustache template, key-value pairs of variables to set for template rendering. Keys must be strings
+   *
+   * the values can be one of the following types:
+   * - string
+   * - number
+   * - boolean
+   * - list, whose values are all of permitted types
+   * - map, whose keys must be strings, and whose values are all of permitted types
+   */
   mustaches?: Record<string, unknown>
 }
