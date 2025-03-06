595fd138d4b936ff97884c433c67a2d8de82bd0c
Yizack Rangel <yizackr@gmail.com>
2025-03-05 20:29:08 -0500

    chore(playground): uncomment recipients

--- playground/server/api/send.ts
+++ playground/server/api/send.ts
@@ -1,8 +1,8 @@
 export default defineEventHandler(async (event) => {
   const mailchannels = useMailChannels(event)
   const response = await mailchannels.send({
-    // from: 'Name From <from@example.com>',
-    // to: 'to@example.com',
+    from: 'Name From <from@example.com>',
+    to: 'to@example.com',
     subject: 'Test',
     html: '<p>Hello {{ world }}</p>',
     text: 'Hello {{ world }}',
